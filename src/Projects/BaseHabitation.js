d197e552d89616934f5dae04d5a126eaca5469cf
Andrew Monthero <andrewmonthero21@gmail.com>
2025-04-20 14:02:58 +0530

    minor changes

--- src/Projects/BaseHabitation.js
+++ src/Projects/BaseHabitation.js
@@ -1,8 +1,11 @@
 const BaseHabitation = () => {
   return (
     <section className="flex h-full flex-col items-center border-t border-[#d4d4d4] py-[4rem]">
+      <h2 className="text-center text-[1.25rem] leading-[1.4] font-bold tracking-tighter md:text-[1.75rem]">
+        Base Habitation
+      </h2>
       <a
-        className="inline-block min-w-[12rem] cursor-pointer rounded-4xl border border-[#d4d4d4] bg-[#fafafa] px-[1.5rem] py-[0.75rem] text-[0.938rem] font-semibold transition-all transition-discrete duration-300 hover:bg-[#0c2320] hover:text-[#fafafa]"
+        className="inline-block min-w-[12rem] cursor-pointer rounded-4xl border border-[#d4d4d4] bg-[#fafafa] px-[1.5rem] py-[0.75rem] text-center text-[0.938rem] font-semibold transition-all transition-discrete duration-300 hover:bg-[#0c2320] hover:text-[#fafafa]"
         href="https://base-habitation.vercel.app"
       >
         Explore
