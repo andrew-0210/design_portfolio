4fbefed81546c16eaead83a45b6f2a4970de5c75
Andrew Monthero <andrewmonthero21@gmail.com>
2025-04-20 13:40:26 +0530

    font weight change

--- src/Projects/UniFive.js
+++ src/Projects/UniFive.js
@@ -2,7 +2,7 @@ const UniFive = () => {
   return (
     <section className="flex h-full flex-col items-center border-t border-[#d4d4d4] py-[4rem]">
       <div className="flex flex-col items-center gap-4 px-[1.5rem] text-center md:max-w-[40rem]">
-        <h2 className="text-[1.25rem] leading-[1.4] font-extrabold tracking-tighter md:text-[1.75rem]">
+        <h2 className="text-[1.25rem] leading-[1.4] font-bold tracking-tighter md:text-[1.75rem]">
           While working with Uni5, I designed a tailored{" "}
           <span className="bg-[#ffdf48] px-[0.5rem]">HRM Platform</span> from
           scratch, automating complex HR workflows and simplifying day-to-day
