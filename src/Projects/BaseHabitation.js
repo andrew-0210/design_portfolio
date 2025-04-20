11324e1407a2eef31afdc369f60a6758ee5b2776
Andrew Monthero <andrewmonthero21@gmail.com>
2025-04-20 13:58:29 +0530

    website added

---
+++ src/Projects/BaseHabitation.js
@@ -0,0 +1,14 @@
+const BaseHabitation = () => {
+  return (
+    <section className="flex h-full flex-col items-center border-t border-[#d4d4d4] py-[4rem]">
+      <a
+        className="inline-block min-w-[12rem] cursor-pointer rounded-4xl border border-[#d4d4d4] bg-[#fafafa] px-[1.5rem] py-[0.75rem] text-[0.938rem] font-semibold transition-all transition-discrete duration-300 hover:bg-[#0c2320] hover:text-[#fafafa]"
+        href="https://base-habitation.vercel.app"
+      >
+        Explore
+      </a>
+    </section>
+  );
+};
+
+export default BaseHabitation;
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,9 +1,11 @@
+import BaseHabitation from "@/Projects/BaseHabitation";
 import UniFive from "@/Projects/UniFive";
 
 const Projects = () => {
   return (
     <section>
       <UniFive />
+      <BaseHabitation />
     </section>
   );
 };
