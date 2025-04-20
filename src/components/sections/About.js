d529f45696ef33d50c7a5f75988ed52c121bf7a4
Andrew Monthero <andrewmonthero21@gmail.com>
2025-04-20 18:56:01 +0530

    Navigation update

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,6 +1,9 @@
 const About = () => {
   return (
-    <section className="flex h-full flex-col items-center py-[2rem] md:py-[4rem]">
+    <section
+      className="flex h-full flex-col items-center py-[2rem] md:py-[4rem]"
+      id="about"
+    >
       <div className="flex flex-col items-center gap-4 px-[1.5rem] text-center md:max-w-[40rem]">
         <p className="md:max-w-[35rem]">
           Currently building modern web experiences for dentists, modular crane
