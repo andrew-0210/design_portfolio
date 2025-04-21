530470f40b130696eb0ac3cf7fde7ac27d00784a
Andrew Monthero <andrewmonthero21@gmail.com>
2025-04-21 16:26:28 +1000

    font-upgrade

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
-import { Manrope, Oooh_Baby } from "next/font/google";
+import { Outfit, Oooh_Baby } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/layout/Navigation";
 import Footer from "@/components/layout/Footer";
 
-const manrope = Manrope({
-  variable: "--font-manrope",
+const outfit = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const shadows = Oooh_Baby({
@@ -24,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${manrope.variable} ${shadows.variable} font-sans antialiased selection:bg-[#ffdf48] selection:text-[#945b41]`}
+        className={`${outfit.variable} ${shadows.variable} font-sans antialiased selection:bg-[#ffdf48] selection:text-[#945b41]`}
       >
         <Navigation />
         {children}
