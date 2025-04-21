937345ccf3989999a564912cf7177b1d0669bc4d
Andrew Monthero <andrewmonthero21@gmail.com>
2025-04-21 18:13:48 +0530

    Minor about changes

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -34,10 +34,10 @@ const About = () => {
           </div>
         </div>
         <p className="md:max-w-[35rem]">
-          Currently building modern web experiences for dentists, modular crane
-          solutions, and pest control services at Onestop Dental Marketing —
-          reshaping traditional industries with better design and stronger
-          digital presence.
+          Currently building modern web experiences for Dentists, Modular Crane
+          Solutions, Duct Cleaning and Pest Control Services at Onestop Dental
+          Marketing — reshaping traditional industries with better design and
+          stronger digital presence.
         </p>
       </div>
     </section>
