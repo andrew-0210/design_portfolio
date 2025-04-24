525c12b89bdab6eb32dd6129cc567aed250df22e
Andrew Monthero <andrewmonthero21@gmail.com>
2025-04-24 18:56:25 +0530

    skyline-heights link updated

--- src/Projects/SkylineHeights.js
+++ src/Projects/SkylineHeights.js
@@ -31,7 +31,7 @@ const SkylineHeights = () => {
         </p>
         {/* <a
         className="inline-block min-w-[12rem] cursor-pointer rounded-4xl border border-[#d4d4d4] bg-[#fafafa] px-[1.5rem] py-[0.75rem] text-center text-[0.938rem] font-semibold transition-all transition-discrete duration-300 hover:bg-[#0c2320] hover:text-[#fafafa]"
-        href="https://base-habitation.vercel.app"
+        href="https://skyline-heights.vercel.app"
       >
         Explore
       </a> */}
