8f6341f97c82aedf6b40eb344930beb6391d9422
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2025-05-26 15:36:58 +0530

    Update Projects.js

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -28,7 +28,7 @@ const Projects = () => {
       <h2 className="text-[1.25rem] leading-[1.4] font-bold tracking-tighter md:text-[1.75rem]">
         Featured Work
       </h2>
-      <section className="flex grid-cols-3 flex-col gap-8 md:grid">
+      <section className="flex grid-cols-2 flex-col gap-8 md:grid">
         {data.map((project, i) => (
           <article className="flex flex-col gap-4" key={i}>
             <div className="relative block h-[20rem] w-auto rounded-2xl bg-[#7d0088]/30 md:h-[40rem]">
