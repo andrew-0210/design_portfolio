652b94ce6abdbfc06673a518409d24071b8302a9
Andrew Monthero <andrewmonthero21@gmail.com>
2025-07-24 12:36:55 +0530

    Projects Updated

---
+++ constants/data.js
@@ -0,0 +1,44 @@
+export const PROJECT_DATA = [
+  {
+    project_name: "Creative Cooks",
+    type: "Case Study",
+    imageUrl: "/images/Creative_cooks_cover.png",
+    href: "https://medium.com/@andrewmonthero/creative-cooks-ui-ux-case-study-5fb14d3332a1",
+  },
+  {
+    project_name: "My Implant Dentist",
+    type: "Website",
+    imageUrl: "/images/Implant_Dentist.png",
+    href: "https://myimplantdentist.com.au/",
+  },
+  {
+    project_name: "HRMS Software",
+    type: "Web App",
+    imageUrl: "/images/Synergia_Showcase.png",
+    href: "https://www.figma.com/design/pTMSRLniQBHdSDsarPBJkV/HRMS-software?node-id=0-1&t=1QOvr8eKl2MpcjEl-1",
+  },
+  {
+    project_name: "School Bus Tracking App",
+    type: "Mobile App",
+    imageUrl: "/images/Synergia_Showcase.png",
+    href: "https://www.figma.com/design/yqnihEG73gYAcTL04pMg7N/Bus-tracking-app?node-id=0-1&t=H0HzfxtGkLeMkfEc-1",
+  },
+  {
+    project_name: "Postsales Software(for managing the selling of flats)",
+    type: "Web App",
+    imageUrl: "/images/Synergia_Showcase.png",
+    href: "https://www.figma.com/design/BngSVkRBOw7cv0yRpBXVRr/BuildNest?node-id=1-2&t=PnAuGjhjTeMoVxG3-1",
+  },
+  {
+    project_name: "Aviation Obstruction Light Monitoring System",
+    type: "Web App",
+    imageUrl: "/images/Synergia_Showcase.png",
+    href: "https://www.figma.com/design/yqnihEG73gYAcTL04pMg7N/Bus-tracking-app?node-id=0-1&t=H0HzfxtGkLeMkfEc-1",
+  },
+  {
+    project_name: "PT Ecological Services - Company Management Software",
+    type: "Web App",
+    imageUrl: "/images/Synergia_Showcase.png",
+    href: "https://www.figma.com/design/BngSVkRBOw7cv0yRpBXVRr/BuildNest?node-id=1-2&t=PnAuGjhjTeMoVxG3-1",
+  },
+];
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,26 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-
-const data = [
-  {
-    project_name: "Creative Cooks",
-    type: "Case Study",
-    imageUrl: "/images/Creative_cooks_cover.png",
-    href: "https://medium.com/@andrewmonthero/creative-cooks-ui-ux-case-study-5fb14d3332a1",
-  },
-  {
-    project_name: "Skyline Heights",
-    type: "Website",
-    imageUrl: "/images/Skyline-Heights__Desktop.png",
-    href: "",
-  },
-  {
-    project_name: "Synergia",
-    type: "Web App",
-    imageUrl: "/images/Synergia_Showcase.png",
-    href: "",
-  },
-];
+import { PROJECT_DATA } from "../../../constants/data";
 
 const Projects = () => {
   return (
@@ -28,18 +8,18 @@ const Projects = () => {
       <h2 className="text-[1.25rem] leading-[1.4] font-bold tracking-tighter md:text-[1.75rem]">
         Featured Work
       </h2>
-      <section className="flex grid-cols-3 flex-col gap-8 md:grid">
-        {data.map((project, i) => (
+      <section className="flex grid-cols-3 flex-col gap-[1.5rem] md:grid">
+        {PROJECT_DATA.map((project, i) => (
           <article className="flex flex-col gap-4" key={i}>
-            <div className="relative block h-[20rem] w-auto overflow-clip rounded-2xl bg-[#7d0088]/30">
+            {/* <div className="relative block h-[20rem] w-auto overflow-clip rounded-2xl bg-[#7d0088]/30">
               <Image
                 src={project.imageUrl}
                 alt={project.project_name}
                 fill
                 className="object-cover"
               />
-            </div>
-            <div className="flex items-center justify-between">
+            </div> */}
+            <div className="flex items-center justify-between rounded-[8px] border border-[#6d6d6d] p-[1.25rem]">
               <div className="flex flex-col gap-1.5">
                 <h3 className="text-[1.125rem] font-medium md:text-[1.25rem]">
                   {project.project_name}
@@ -50,12 +30,13 @@ const Projects = () => {
               </div>
               <div>
                 {project?.href && (
-                  <Link
+                  <a
                     href={project.href}
                     className="inline-flex items-center justify-center rounded-full border bg-white px-[1.25rem] py-[0.25rem] text-[1.5rem] font-semibold shadow-[0_4px_0px_rgba(0,0,0,1)] transition-shadow duration-200 ease-in hover:shadow-none"
+                    target="_blank"
                   >
                     &rarr;
-                  </Link>
+                  </a>
                 )}
               </div>
             </div>
